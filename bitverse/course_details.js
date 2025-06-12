0c49d0ee0c0ebb6b5123e10df79c880d8f0191a2
0sureshyadav0 <suresh.y.np@gmail.com>
2025-06-12 12:51:01 +0545

    bitverse added

---
+++ bitverse/course_details.js
@@ -0,0 +1,8 @@
+const courseDetails = [
+  {
+    packageName: "Basics of C Programming",
+    content: "Basics of C, syntax, data types, loops, functions and arrays",
+    Mode: "Recorded or Live",
+    price: "Rs. 1,000 - Rs, 2,500",
+  },
+];
---
+++ bitverse/courses.js
@@ -0,0 +1,151 @@
+const courses = [
+  {
+    title: "C Language",
+    description:
+      "Clear understanding of C programming fundamentals which includes variables, data types, control structures, functions, and arrays.",
+    duration: "1 month (3-4 hr/week)",
+    originalPrice: "4,000",
+    discountedPrice: "1,499",
+    batchNote: "For 1<sup>st</sup> batch",
+    image: "./assets/images/c.png",
+
+    allpackages: [
+      {
+        packageName: "Basics of C Programming",
+        content: "Basics of C, syntax, data types, loops, functions and arrays",
+        mode: "Recorded or Live",
+        duration: "1 month (3-4 hr/week)",
+        price: "Rs. 1,000 - Rs, 2,500",
+      },
+      {
+        packageName: "Standard Package (Intermediate)",
+        content:
+          "All basics, arrays, pointers, file handling and mini projects",
+        mode: "Recorded or Live",
+        duration: "1.5-2 month ",
+        bonus: "PDF notes",
+        price: "Rs. 3,000 - Rs, 5,000",
+      },
+      {
+        packageName: "Premium Package (Advanced)",
+        content:
+          "Full C masters, advanced topics, projects (quiz app, file encryptor)",
+        mode: "Recorded or Live",
+        duration: "1.5-2 month ",
+        bonus: "PDF notes",
+        price: "Rs. 3,000 - Rs, 5,000",
+      },
+    ],
+  },
+  {
+    title: "Python for Beginners",
+    description:
+      "Learn Python from scratch with real-world examples and live coding sessions. It includes variables, data types, operators, conditions, loops, functions, Basic list/dictionary use.",
+    duration: "1 month (12-16 hours total)",
+    originalPrice: "5,000",
+    discountedPrice: "4,499",
+    batchNote: "For 1<sup>st</sup> batch",
+    image: "./assets/images/python.png",
+    allpackages: [
+      {
+        packageName: "Basics of C Programming",
+        content: "Basics of C, syntax, data types, loops, functions and arrays",
+        mode: "Recorded or Live",
+        duration: "1 month (3-4 hr/week)",
+        price: "Rs. 1,000 - Rs, 2,500",
+      },
+      {
+        packageName: "Standard Package (Intermediate)",
+        content:
+          "All basics, arrays, pointers, file handling and mini projects",
+        mode: "Recorded or Live",
+        duration: "1.5-2 month ",
+        bonus: "PDF notes",
+        price: "Rs. 3,000 - Rs, 5,000",
+      },
+      {
+        packageName: "Premium Package (Advanced)",
+        content:
+          "Full C masters, advanced topics, projects (quiz app, file encryptor)",
+        mode: "Recorded or Live",
+        duration: "1.5-2 month ",
+        bonus: "PDF notes",
+        price: "Rs. 3,000 - Rs, 5,000",
+      },
+    ],
+  },
+  {
+    title: "Web Development Bootcamp",
+    description:
+      "Learn to develop websites. It includes HTML, CSS, Basic JavaScript,(DOM, events, forms)",
+    duration: "1 month (12-16 hours total)",
+    originalPrice: "5,000",
+    discountedPrice: "4,499",
+    batchNote: "For 1<sup>st</sup> batch",
+    image: "./assets/images/webdev.png",
+    allpackages: [
+      {
+        packageName: "Basics of C Programming",
+        content: "Basics of C, syntax, data types, loops, functions and arrays",
+        mode: "Recorded or Live",
+        duration: "1 month (3-4 hr/week)",
+        price: "Rs. 1,000 - Rs, 2,500",
+      },
+      {
+        packageName: "Standard Package (Intermediate)",
+        content:
+          "All basics, arrays, pointers, file handling and mini projects",
+        mode: "Recorded or Live",
+        duration: "1.5-2 month ",
+        bonus: "PDF notes",
+        price: "Rs. 3,000 - Rs, 5,000",
+      },
+      {
+        packageName: "Premium Package (Advanced)",
+        content:
+          "Full C masters, advanced topics, projects (quiz app, file encryptor)",
+        mode: "Recorded or Live",
+        duration: "1.5-2 month ",
+        bonus: "PDF notes",
+        price: "Rs. 3,000 - Rs, 5,000",
+      },
+    ],
+  },
+  {
+    title: "App Development - Flutter",
+    description:
+      "Build beautiful cross-platform mobile apps using Flutter and Dart. Learn widgets, state management, and Firebase integration to create powerful Android and iOS apps.",
+    duration: "1.5-2 months (30-40 hours total)",
+    originalPrice: "18,000",
+    discountedPrice: "14,999",
+    batchNote: "For 1<sup>st</sup> batch",
+    image: "./assets/images/flutter.png",
+    allpackages: [
+      {
+        packageName: "Basics of C Programming",
+        content: "Basics of C, syntax, data types, loops, functions and arrays",
+        mode: "Recorded or Live",
+        duration: "1 month (3-4 hr/week)",
+        price: "Rs. 1,000 - Rs, 2,500",
+      },
+      {
+        packageName: "Standard Package (Intermediate)",
+        content:
+          "All basics, arrays, pointers, file handling and mini projects",
+        mode: "Recorded or Live",
+        duration: "1.5-2 month ",
+        bonus: "PDF notes",
+        price: "Rs. 3,000 - Rs, 5,000",
+      },
+      {
+        packageName: "Premium Package (Advanced)",
+        content:
+          "Full C masters, advanced topics, projects (quiz app, file encryptor)",
+        mode: "Recorded or Live",
+        duration: "1.5-2 month ",
+        bonus: "PDF notes",
+        price: "Rs. 3,000 - Rs, 5,000",
+      },
+    ],
+  },
+];
